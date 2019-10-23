fccf9707e10ddc48fa61cf00bfa48720f4325ec5
FreezerNick <nl@f2p-entertainment.de>
2019-10-23 11:25:46 +0200

    Chart Display; #1

--- Code/WebserverTest3/index.h
+++ Code/WebserverTest3/index.h
@@ -56,86 +56,88 @@ const char MAIN_page[] PROGMEM = R"=====(
 <br>  
 
 <script>
-var values = data;
-var timeStamp = time                          ;
-function showGraph()
-{
-    for (i = 0; i < arguments.length; i++) {
-      values.push(arguments[i]);    
-    }
-
-    var ctx = document.getElementById("Chart").getContext('2d');
-    var Chart2 = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: timeStamp,  //Bottom Labeling
-            datasets: [{
-                label: "Hoehe",
-                fill: false,  //Try with true
-                backgroundColor: 'rgba( 243, 156, 18 , 1)', //Dot marker color
-                borderColor: 'rgba( 243, 156, 18 , 1)', //Graph Line Color
-                data: values,
-            }],
-        },
-        options: {
-            title: {
-                    display: true,
-                    text: "Hoehe"
-                },
-            maintainAspectRatio: false,
-            elements: {
-            line: {
-                    tension: 0.5 //Smoothening (Curved) of data lines
-                }
-            },
-            scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true
-                        }
-                    }]
-            }
-        }
-    });
-
-}
-
-//On Page load show graphs
-window.onload = function() {
-  console.log(new Date().toLocaleTimeString());
-  showGraph(5,10,4,58);
-};
-
-//Ajax script to get ADC voltage at every 5 Seconds 
-//Read This tutorial https://circuits4you.com/2018/02/04/esp8266-ajax-update-part-of-web-page-without-refreshing/
-
-setInterval(function() {
-  // Call a function repetatively with 5 Second interval
-  getData();
-}, 5000); //5000mSeconds update rate
-
-function getData() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     //Push the data in array
+var data = [];
   var time = new Date().toLocaleTimeString();
-  var data = this.responseText; 
-      values.push(data);
-      timeStamp.push(time);
-      showGraph();  //Update Graphs
-  //Update Data Table
-    var table = document.getElementById("dataTable");
-    var row = table.insertRow(1); //Add after headings
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = time;
-    cell2.innerHTML = data;
-    }
+  var values = data;
+  var timeStamp = time;
+  function showGraph()
+  {
+      for (i = 0; i < arguments.length; i++) {
+        values.push(arguments[i]);    
+      }
+  
+      var ctx = document.getElementById("Chart").getContext('2d');
+      var Chart2 = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: [timeStamp],  //Bottom Labeling
+              datasets: [{
+                  label: "Hoehe",
+                  fill: false,  //Try with true
+                  backgroundColor: 'rgba( 243, 156, 18 , 1)', //Dot marker color
+                  borderColor: 'rgba( 243, 156, 18 , 1)', //Graph Line Color
+                  data: values,
+              }],
+          },
+          options: {
+              title: {
+                      display: true,
+                      text: "Hoehe"
+                  },
+              maintainAspectRatio: false,
+              elements: {
+              line: {
+                      tension: 0.5 //Smoothening (Curved) of data lines
+                  }
+              },
+              scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero:true
+                          }
+                      }]
+              }
+          }
+      });
+  
+  }
+  
+  //On Page load show graphs
+  window.onload = function() {
+    console.log(new Date().toLocaleTimeString());
+    showGraph(5,10,4,58);
   };
-  xhttp.open("GET", "readData", true);  //Handle readADC server on ESP8266
-  xhttp.send();
-}
+  
+  //Ajax script to get ADC voltage at every 5 Seconds 
+  //Read This tutorial https://circuits4you.com/2018/02/04/esp8266-ajax-update-part-of-web-page-without-refreshing/
+  
+  setInterval(function() {
+    // Call a function repetatively with 5 Second interval
+    getData();
+  }, 5000); //5000mSeconds update rate
+  
+  function getData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       //Push the data in array
+    var time = new Date().toLocaleTimeString();
+    data = this.responseText; 
+        values.push(data);
+        timeStamp.push(time);
+        showGraph();  //Update Graphs
+    //Update Data Table
+      var table = document.getElementById("dataTable");
+      var row = table.insertRow(1); //Add after headings
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = time;
+      cell2.innerHTML = data;
+      }
+    };
+    xhttp.open("GET", "readData", true);  //Handle readADC server on ESP8266
+    xhttp.send();
+  }
 
 </script>
 </body>
