bcded35009e3ddd2ec3ac27e0d86295880a94325
JantonDeluxe <54102161+JantonDeluxe@users.noreply.github.com>
2019-11-26 14:01:57 +0100

    Update anzeige.ino

--- Code/Hoehenmesser/anzeige.ino
+++ Code/Hoehenmesser/anzeige.ino
@@ -8,33 +8,33 @@ void anzeige() {
     oled.setCursor(0,5);
     oled.print("Max:");
 
-    // Höhenunterschied anzeigen
+    // Höhenunterschied
     oled.set2X();
     oled.setCursor(40, 2);               
     if (h >= 0.0) oled.print(" ");      
     oled.print(h);
     oled.print("m");
 
-    // Maximum anzeigen
+    // Maximum
     oled.setCursor(40, 4);               
     if (highest >= 0.0) oled.print(" ");
     oled.print(highest);
     oled.print("m");
     oled.set1X();
 
-    // Druckanzeige
+    // Druck
     oled.setCursor(0, 0);
     oled.print(getPressure());
     oled.print(" mbar");
 
-    // Temperaturanzeige
+    // Temperatur
     oled.setCursor(79, 0);
     if (a >= 0.0) oled.print(" ");
     oled.print(T);
     oled.setCursor(115, 0);
     oled.println(" C");
 
-    // IP-Adresse anzeigen
+    // IP-Adresse
     oled.setCursor(0, 7);
     oled.print("IP: ");
 
